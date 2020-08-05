b543ac09e1ecdacae1cfb22ce64052c63eba69d0
JesterJz <zimmer.jeyy@gmail.com>
2020-08-05 15:11:00 +0700

    commit vo van

--- app/src/main/java/com/build/myapplication/Service/APIRetrofitClient.java
+++ app/src/main/java/com/build/myapplication/Service/APIRetrofitClient.java
@@ -10,25 +10,26 @@ import okhttp3.OkHttpClient;
 import okhttp3.Protocol;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
 //tuong tac len service
 public class APIRetrofitClient {
     private static Retrofit retrofit = null;
 
-    public static Retrofit getClient(String base_url){
+    public static Retrofit getClient(String base_url) {
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                                                    .readTimeout(10000, TimeUnit.MILLISECONDS)
-                                                    .writeTimeout(10000,TimeUnit.MILLISECONDS)
-                                                    .connectTimeout(10000,TimeUnit.MILLISECONDS)
-                                                    .retryOnConnectionFailure(true)
-                                                    .protocols(Arrays.asList(Protocol.HTTP_1_1))
-                                                    .build();
+                .readTimeout(10000, TimeUnit.MILLISECONDS)
+                .writeTimeout(10000, TimeUnit.MILLISECONDS)
+                .connectTimeout(10000, TimeUnit.MILLISECONDS)
+                .retryOnConnectionFailure(true)
+                .protocols(Arrays.asList(Protocol.HTTP_1_1))
+                .build();
         Gson gson = new GsonBuilder().setLenient().create();
         retrofit = new Retrofit.Builder()
-                                .baseUrl(base_url)
-                                .client(okHttpClient)
-                                .addConverterFactory(GsonConverterFactory.create(gson))
-                                .build();
-        return  retrofit;
+                .baseUrl(base_url)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        return retrofit;
     }
 
 }
--- app/src/main/java/com/build/myapplication/Service/APIService.java
+++ app/src/main/java/com/build/myapplication/Service/APIService.java
@@ -3,7 +3,7 @@ package com.build.myapplication.Service;
 public class APIService {
     private static String base_url = "https://jestermusic.tk/Server/";
 
-    public static DataService getService(){
+    public static DataService getService() {
         return APIRetrofitClient.getClient(base_url).create(DataService.class);
     }
 }
